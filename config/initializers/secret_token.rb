d635e9dd5d0e84502e6021b261283d88b1d4082c
Franz Amador <fgamador@yahoo.com>
2010-12-24 16:19:30 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Badger::Application.config.secret_token = 'ed9a1e34dc5d98490f7da91e730bf0c6263b11c8877a05e0a3e34401c319efab882afac07cf0ce9d5dcfd24420e519d7241da3418352f6fefa475180ddd6f929'
