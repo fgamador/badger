b6d82ae8cd668e2cddc36ff6bf706cf3dd453b18
Franz Amador <fgamador@yahoo.com>
2011-01-06 20:37:42 -0800

    fill in award and outing models

--- app/models/court_of_honor.rb
+++ app/models/court_of_honor.rb
@@ -1,2 +1,9 @@
 class CourtOfHonor < ActiveRecord::Base
+  validates_presence_of :name, :date
+  validates_uniqueness_of :name
+  validates_uniqueness_of :date
+#  has_many :scout_awards, :dependent => :nullify
+#  has_many :scout_merit_badges, :dependent => :nullify
+#  has_many :scout_ranks, :dependent => :nullify
 end
+
--- app/models/outing.rb
+++ app/models/outing.rb
@@ -1,2 +1,9 @@
 class Outing < ActiveRecord::Base
+  validates_presence_of :name, :date, :nights_of_camping
+  validates_uniqueness_of :name
+#  has_many :scout_outings, :dependent => :destroy
+#  has_many :scouts, :through => :scout_outings
+#  has_many :outing_awards, :dependent => :destroy
+#  has_many :awards, :through => :outing_awards, :order => "name"
 end
+
