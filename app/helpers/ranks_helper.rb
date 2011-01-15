a8c9a3fc0e6f9fd8aca08c7cfce16efda89fad78
Franz Amador <fgamador@yahoo.com>
2011-01-15 14:20:51 -0800

    rank tweaks

--- app/helpers/ranks_helper.rb
+++ app/helpers/ranks_helper.rb
@@ -2,7 +2,9 @@ module RanksHelper
   def td_rank_merit_badge_counts(rank)
     safe_concat "<td title='#{rank.num_merit_badges} total, #{rank.num_eagle_required} eagle-required'>"
     if rank.num_merit_badges > 0
-      safe_concat "#{rank.num_merit_badges} (#{rank.num_eagle_required})"
+      concat "#{rank.num_merit_badges} (#{rank.num_eagle_required})"
+    else
+      concat "-"
     end
     safe_concat "</td>"
   end
